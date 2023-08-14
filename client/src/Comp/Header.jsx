928625a3443055ac72d52ccbdf24a94d13b15c23
Geetansh Rahangdale <geetansh335@gmail.com>
2023-08-15 01:55:41 +0530

    logout issue solved

--- client/src/Comp/Header.jsx
+++ client/src/Comp/Header.jsx
@@ -18,6 +18,12 @@ const Header = () => {
     axios.post("http://localhost:5000/logout",{
         withCredentials : 'include'
     });
+    var allCookies = document.cookie.split(";");
+    // The "expire" attribute of every cookie is
+    // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+    for (var i = 0; i < allCookies.length; i++)
+      document.cookie =
+        allCookies[i] + "=;expires=" + new Date(0).toUTCString();
     setUserInfo(null);
   }
   const UserName = userInfo?.data?.userName;
